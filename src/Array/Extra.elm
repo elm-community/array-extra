de0b193cf6353ebacebfaea96f070e3a735f3938
jheiling <juergen.heiling@gmail.com>
2015-04-02 00:17:59 +0200

    added getOr

--- src/Array/Extra.elm
+++ src/Array/Extra.elm
@@ -1,6 +1,9 @@
 module Array.Extra where
 {-| Convenience functions for working with Array
 
+# Getter
+@docs getOr
+
 # Higher order helpers
 @docs filterMap, apply, map2, map3, map4, map5
 
@@ -24,6 +27,14 @@ getUnsafe n xs =
   case get n xs of
     Just x -> x
     Nothing -> Debug.crash ("Index " ++ toString n ++ " of Array with length " ++ toString (length xs) ++ " is not reachable.")
+
+{-| Return the element at the index or a default if the index is out of range.
+-}
+getOr : Int -> a -> Array a -> a
+getOr n val xs =
+  case get n xs of
+    Just x -> x
+    Nothing -> val
 
 {-| Apply a function that may succeed to all values in the array, but only keep the successes.
 
