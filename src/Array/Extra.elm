fda7fc9c8186cbc075662a085e4ca73efae84d1e
Jonathan Boiser <jonathan.boiser@monsanto.com>
2016-05-19 10:22:14 -0500

    elm-format Extra.elm

--- src/Array/Extra.elm
+++ src/Array/Extra.elm
@@ -1,4 +1,5 @@
 module Array.Extra exposing (..)
+
 {-| Convenience functions for working with Array
 
 # Transformations
@@ -31,12 +32,16 @@ import Debug
 -}
 update : Int -> (a -> a) -> Array a -> Array a
 update n f a =
-  let
-    element = Array.get n a
-  in
-    case element of
-      Nothing -> a
-      Just element' -> Array.set n (f element') a
+    let
+        element =
+            Array.get n a
+    in
+        case element of
+            Nothing ->
+                a
+
+            Just element' ->
+                Array.set n (f element') a
 
 
 {-| Drop *n* first elements from an array. In other words, slice an array from an index until the very end. Given negative argument, count the end of the slice from the end of the array.
@@ -45,7 +50,8 @@ update n f a =
     sliceFrom -3 (fromList [0..9]) == fromList [7,8,9]
 -}
 sliceFrom : Int -> Array a -> Array a
-sliceFrom n a = slice n (length a) a
+sliceFrom n a =
+    slice n (length a) a
 
 
 {-| Take *n* first elements from an array. In other words, slice an array from the very beginning until index not including. Given negative argument, count the beginning of the slice from the end of the array.
@@ -55,18 +61,23 @@ sliceFrom n a = slice n (length a) a
 -}
 sliceUntil : Int -> Array a -> Array a
 sliceUntil n a =
-  if n >= 0
-  then slice 0 n a
-  else slice 0 (length a + n) a
+    if n >= 0 then
+        slice 0 n a
+    else
+        slice 0 (length a + n) a
 
 
 {-| Unsafe version of get, don't use this unless you know what you're doing!
 -}
 getUnsafe : Int -> Array a -> a
 getUnsafe n xs =
-  case get n xs of
-    Just x -> x
-    Nothing -> Debug.crash ("Index " ++ toString n ++ " of Array with length " ++ toString (length xs) ++ " is not reachable.")
+    case get n xs of
+        Just x ->
+            x
+
+        Nothing ->
+            Debug.crash ("Index " ++ toString n ++ " of Array with length " ++ toString (length xs) ++ " is not reachable.")
+
 
 {-| Apply a function that may succeed to all values in the array, but only keep the successes.
 
@@ -75,12 +86,19 @@ getUnsafe n xs =
 -}
 filterMap : (a -> Maybe b) -> Array a -> Array b
 filterMap f xs =
-  let maybePush : (a -> Maybe b) -> a -> Array b -> Array b
-      maybePush f mx xs =
-          case f mx of
-            Just x -> push x xs
-            Nothing -> xs
-  in foldl (maybePush f) empty xs
+    let
+        maybePush : (a -> Maybe b) -> a -> Array b -> Array b
+        maybePush f mx xs =
+            case f mx of
+                Just x ->
+                    push x xs
+
+                Nothing ->
+                    xs
+    in
+        foldl (maybePush f) empty xs
+
+
 
 -- {-| Select the longest one of several arrays (similar to `Maybe.oneOf`).
 --
@@ -109,13 +127,20 @@ filterMap f xs =
 -- lengthierOf : Array a -> Array a -> Array a
 -- lengthierOf x y = if Array.length y < Array.length x then y else x
 
+
 {-| Apply an array of functions to an array of values.
 -}
 apply : Array (a -> b) -> Array a -> Array b
 apply fs xs =
-  let l              = min (length fs) (length xs)
-      fs'            = slice 0 l fs
-  in  indexedMap (\n f -> f (getUnsafe n xs)) fs'
+    let
+        l =
+            min (length fs) (length xs)
+
+        fs' =
+            slice 0 l fs
+    in
+        indexedMap (\n f -> f (getUnsafe n xs)) fs'
+
 
 {-| Combine two arrays, combining them with the given function.
 If one array is longer, the extra elements are dropped.
@@ -127,19 +152,26 @@ If one array is longer, the extra elements are dropped.
         map2 (,) lefts rights
 -}
 map2 : (a -> b -> result) -> Array a -> Array b -> Array result
-map2 f ws = apply (map f ws)
+map2 f ws =
+    apply (map f ws)
 
-{-|-}
+
+{-| -}
 map3 : (a -> b -> c -> result) -> Array a -> Array b -> Array c -> Array result
-map3 f ws xs = apply (map2 f ws xs)
+map3 f ws xs =
+    apply (map2 f ws xs)
 
-{-|-}
+
+{-| -}
 map4 : (a -> b -> c -> d -> result) -> Array a -> Array b -> Array c -> Array d -> Array result
-map4 f ws xs ys = apply (map3 f ws xs ys)
+map4 f ws xs ys =
+    apply (map3 f ws xs ys)
 
-{-|-}
+
+{-| -}
 map5 : (a -> b -> c -> d -> e -> result) -> Array a -> Array b -> Array c -> Array d -> Array e -> Array result
-map5 f ws xs ys zs = apply (map4 f ws xs ys zs)
+map5 f ws xs ys zs =
+    apply (map4 f ws xs ys zs)
 
 
 {-| Take a predicate and an array, return an array that contains elements which fails to satisfy the predicate.
@@ -149,111 +181,160 @@ map5 f ws xs ys zs = apply (map4 f ws xs ys zs)
 -}
 removeWhen : (a -> Bool) -> Array a -> Array a
 removeWhen pred xs =
-  Array.filter (not << pred) xs
+    Array.filter (not << pred) xs
 
 
 {-| Zip arrays into tuples
 -}
-zip : Array a -> Array b -> Array (a,b)
-zip = map2 (,)
+zip : Array a -> Array b -> Array ( a, b )
+zip =
+    map2 (,)
 
-{-|-}
-zip3 : Array a -> Array b -> Array c -> Array (a,b,c)
-zip3 = map3 (,,)
 
-{-|-}
-zip4 : Array a -> Array b -> Array c -> Array d -> Array (a,b,c,d)
-zip4 = map4 (,,,)
+{-| -}
+zip3 : Array a -> Array b -> Array c -> Array ( a, b, c )
+zip3 =
+    map3 (,,)
 
-{-|-}
-zip5 : Array a -> Array b -> Array c -> Array d -> Array e -> Array (a,b,c,d,e)
-zip5 = map5 (,,,,)
+
+{-| -}
+zip4 : Array a -> Array b -> Array c -> Array d -> Array ( a, b, c, d )
+zip4 =
+    map4 (,,,)
+
+
+{-| -}
+zip5 : Array a -> Array b -> Array c -> Array d -> Array e -> Array ( a, b, c, d, e )
+zip5 =
+    map5 (,,,,)
+
 
 {-| Reduce a list from the left, building up all of the intermediate results into a list.
 -}
---scanl : (a -> b -> b) -> b -> List a -> List b
 
+
+
+--scanl : (a -> b -> b) -> b -> List a -> List b
 --scanl (+) 0 [1,2,3,4] == [0,1,3,6,10]
 --scanl1 : (a -> a -> a) -> List a -> List a
 --Same as scanl but it doesn't require a base case. List must be non-empty.
-
 --scanl1 (+) [1,2,3,4] == [1,3,6,10]
-
 -- {-| Resize an array by unfolding from the left (pad values onto the right-hand side)
 -- -}
 -- resizel : Int -> (a -> a -> a) -> a -> Array a -> Array a
 -- resizel n f =
-
 -- {-| Resize an array by unfolding from the right (pad values onto the left-hand side)
 -- -}
 -- resizer : Int -> (a -> a -> a) -> a -> Array a -> Array a
 -- resizer n f =
 
+
 {-| Resize an array from the left, padding the right-hand side with the given value.
 -}
 resizelRepeat : Int -> a -> Array a -> Array a
 resizelRepeat n val xs =
-  let l = length xs
-  in  if l > n
-      then slice 0 n xs
-      else if l < n
-      then xs `append` repeat (n - l) val
-      else xs
+    let
+        l =
+            length xs
+    in
+        if l > n then
+            slice 0 n xs
+        else if l < n then
+            xs `append` repeat (n - l) val
+        else
+            xs
+
 
 {-| Resize an array from the right, padding the left-hand side with the given value.
 -}
 resizerRepeat : Int -> a -> Array a -> Array a
 resizerRepeat n val xs =
-  let l = length xs
-  in  if l > n
-      then slice (l - n) l xs
-      else if l < n
-      then repeat (n - l) val `append` xs
-      else xs
+    let
+        l =
+            length xs
+    in
+        if l > n then
+            slice (l - n) l xs
+        else if l < n then
+            repeat (n - l) val `append` xs
+        else
+            xs
+
 
 {-| Resize an array from the left, padding the right-hand side with the given index function.
 -}
 resizelIndexed : Int -> (Int -> a) -> Array a -> Array a
 resizelIndexed n f xs =
-  let l     = length xs
-      gen m g = indexedMap (\i _ -> g i) <| repeat m ()
-  in if l > n
-     then slice 0 n xs
-     else if l < n
-     then xs `append` gen (n - l) (f << (\i -> i + l))
-     else xs
+    let
+        l =
+            length xs
+
+        gen m g =
+            indexedMap (\i _ -> g i) <| repeat m ()
+    in
+        if l > n then
+            slice 0 n xs
+        else if l < n then
+            xs `append` gen (n - l) (f << (\i -> i + l))
+        else
+            xs
+
 
 {-| Resize an array from the right, padding the left-hand side with the given index function.
 -}
 resizerIndexed : Int -> (Int -> a) -> Array a -> Array a
 resizerIndexed n f xs =
-  let l = length xs
-      gen m g = indexedMap (\i _ -> g i) <| repeat m ()
-  in  if l > n
-      then slice (l - n) l xs
-      else if l < n
-      then gen (n - l) f `append` xs
-      else xs
+    let
+        l =
+            length xs
+
+        gen m g =
+            indexedMap (\i _ -> g i) <| repeat m ()
+    in
+        if l > n then
+            slice (l - n) l xs
+        else if l < n then
+            gen (n - l) f `append` xs
+        else
+            xs
+
 
 {-| Split an array into two arrays, the first ending at and the second starting at the given index
 -}
-splitAt : Int -> Array a -> (Array a, Array a)
+splitAt : Int -> Array a -> ( Array a, Array a )
 splitAt index xs =
-  -- TODO: refactor (written this way to help avoid Array bugs)
-  let len = length xs
-  in case (index > 0, index < len) of
-    (True,  True ) -> (slice 0 index xs, slice index len xs)
-    (True,  False) -> (xs,               empty)
-    (False, True ) -> (empty,            xs)
-    (False, False) -> (empty,            empty)
+    -- TODO: refactor (written this way to help avoid Array bugs)
+    let
+        len =
+            length xs
+    in
+        case ( index > 0, index < len ) of
+            ( True, True ) ->
+                ( slice 0 index xs, slice index len xs )
+
+            ( True, False ) ->
+                ( xs, empty )
+
+            ( False, True ) ->
+                ( empty, xs )
+
+            ( False, False ) ->
+                ( empty, empty )
+
 
 {-| Remove the element at the given index
 -}
 removeAt : Int -> Array a -> Array a
 removeAt index xs =
-  -- TODO: refactor (written this way to help avoid Array bugs)
-  let (xs0, xs1) = splitAt index xs
-      len1       = length xs1
-  in  if len1 == 0
-      then xs0
-      else xs0 `append` slice 1 len1 xs1
+    -- TODO: refactor (written this way to help avoid Array bugs)
+    let
+        ( xs0, xs1 ) =
+            splitAt index xs
+
+        len1 =
+            length xs1
+    in
+        if len1 == 0 then
+            xs0
+        else
+            xs0 `append` slice 1 len1 xs1
