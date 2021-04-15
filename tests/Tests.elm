cf4442450645eb3c91954eddc33d638d39a289a2
passi <pascalfeyer@gmail.com>
2021-04-15 17:32:07 +0200

    add pop test, reformat tests

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,6 +1,6 @@
 module Tests exposing (..)
 
-import Array exposing (fromList)
+import Array exposing (empty, fromList)
 import Array.Extra exposing (..)
 import Expect exposing (Expectation)
 import Test exposing (..)
@@ -13,25 +13,44 @@ suite =
             (\() ->
                 fromList [ 1, 2, 3 ]
                     |> Expect.all
-                        [ update 1 ((+) 10) >> Expect.equal (fromList [ 1, 12, 3 ])
-                        , update 4 ((+) 10) >> Expect.equal (fromList [ 1, 2, 3 ])
-                        , update -1 ((+) 10) >> Expect.equal (fromList [ 1, 2, 3 ])
+                        [ update 1 ((+) 10)
+                            >> Expect.equal (fromList [ 1, 12, 3 ])
+                        , update 4 ((+) 10)
+                            >> Expect.equal (fromList [ 1, 2, 3 ])
+                        , update -1 ((+) 10)
+                            >> Expect.equal (fromList [ 1, 2, 3 ])
                         ]
             )
         , test "sliceFrom"
             (\() ->
                 fromList (List.range 0 9)
                     |> Expect.all
-                        [ sliceFrom 5 >> Expect.equal (fromList [ 5, 6, 7, 8, 9 ])
-                        , sliceFrom -3 >> Expect.equal (fromList [ 7, 8, 9 ])
+                        [ sliceFrom 5
+                            >> Expect.equal (fromList [ 5, 6, 7, 8, 9 ])
+                        , sliceFrom -3
+                            >> Expect.equal (fromList [ 7, 8, 9 ])
                         ]
             )
         , test "sliceUntil"
             (\() ->
                 fromList (List.range 0 9)
                     |> Expect.all
-                        [ sliceUntil 5 >> Expect.equal (fromList [ 0, 1, 2, 3, 4 ])
-                        , sliceUntil -3 >> Expect.equal (fromList [ 0, 1, 2, 3, 4, 5, 6 ])
+                        [ sliceUntil 5
+                            >> Expect.equal (fromList [ 0, 1, 2, 3, 4 ])
+                        , sliceUntil -3
+                            >> Expect.equal (fromList [ 0, 1, 2, 3, 4, 5, 6 ])
                         ]
+            )
+        , test "pop"
+            (\() ->
+                Expect.all
+                    [ \() ->
+                        pop (fromList [ 1, 2, 3 ])
+                            |> Expect.equal (fromList [ 1, 2 ])
+                    , \() ->
+                        pop empty
+                            |> Expect.equal empty
+                    ]
+                    ()
             )
         ]
