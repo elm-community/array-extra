89d8f07c8b118e693a619e5b5ba7d69053c97140
passi <pascalfeyer@gmail.com>
2021-04-15 17:26:31 +0200

    add sliceFrom test

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,4 +1,4 @@
-module Example exposing (..)
+module Tests exposing (..)
 
 import Array exposing (fromList)
 import Array.Extra exposing (..)
@@ -16,6 +16,14 @@ suite =
                         [ update 1 ((+) 10) >> Expect.equal (fromList [ 1, 12, 3 ])
                         , update 4 ((+) 10) >> Expect.equal (fromList [ 1, 2, 3 ])
                         , update -1 ((+) 10) >> Expect.equal (fromList [ 1, 2, 3 ])
+                        ]
+            )
+        , test "sliceFrom"
+            (\() ->
+                fromList (List.range 0 9)
+                    |> Expect.all
+                        [ sliceFrom 5 >> Expect.equal (fromList [ 5, 6, 7, 8, 9 ])
+                        , sliceFrom -3 >> Expect.equal (fromList [ 7, 8, 9 ])
                         ]
             )
         ]
