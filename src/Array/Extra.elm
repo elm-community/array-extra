570d2541c30d3f05b2c4dc41d973b6877e8a72d8
passi <pascalfeyer@gmail.com>
2021-04-16 18:26:27 +0200

    change formatting, change argument names

--- src/Array/Extra.elm
+++ src/Array/Extra.elm
@@ -34,11 +34,14 @@ import Array exposing (..)
 
 {-| Update the element at the index using a function. Returns the array unchanged if the index is out of bounds.
 
-    update 1 ((+) 10) (fromList [ 1, 2, 3 ]) == fromList [ 1, 12, 3 ]
+    update 1 ((+) 10) (fromList [ 1, 2, 3 ])
+        == fromList [ 1, 12, 3 ]
 
-    update 4 ((+) 10) (fromList [ 1, 2, 3 ]) == fromList [ 1, 2, 3 ]
+    update 4 ((+) 10) (fromList [ 1, 2, 3 ])
+        == fromList [ 1, 2, 3 ]
 
-    update -1 ((+) 10) (fromList [ 1, 2, 3 ]) == fromList [ 1, 2, 3 ]
+    update -1 ((+) 10) (fromList [ 1, 2, 3 ])
+        == fromList [ 1, 2, 3 ]
 
 -}
 update : Int -> (a -> a) -> Array a -> Array a
@@ -55,42 +58,52 @@ update n f a =
             Array.set n (f element_) a
 
 
-{-| Drop _n_ first elements from an array. In other words, slice an array from an index until the very end. Given negative argument, count the end of the slice from the end of the array.
+{-| Drop _n_ first elements from an array.
+In other words, slice an array from an index until the very end.
+Given negative argument, count the end of the slice from the end of the array.
 
-    sliceFrom 5 (fromList (List.range 0 9)) == fromList [ 5, 6, 7, 8, 9 ]
+    sliceFrom 3 (fromList (List.range 0 6))
+        == fromList [ 3, 4, 5, 6 ]
 
-    sliceFrom -3 (fromList (List.range 0 9)) == fromList [ 7, 8, 9 ]
+    sliceFrom -3 (fromList (List.range 0 6))
+        == fromList [ 4, 5, 6 ]
 
 -}
 sliceFrom : Int -> Array a -> Array a
-sliceFrom n a =
-    slice n (length a) a
+sliceFrom n array =
+    slice n (length array) array
 
 
-{-| Take _n_ first elements from an array. In other words, slice an array from the very beginning until index not including. Given negative argument, count the beginning of the slice from the end of the array.
+{-| Take _n_ first elements from an array.
+In other words, slice an array from the very beginning until index not including.
+Given negative argument, count the beginning of the slice from the end of the array.
 
-    sliceUntil 5 (fromList (List.range 0 9)) == fromList [ 0, 1, 2, 3, 4 ]
+    sliceUntil 3 (fromList (List.range 0 6))
+        == fromList [ 0, 1, 2 ]
 
-    sliceUntil -3 (fromList (List.range 0 9)) == fromList [ 0, 1, 2, 3, 4, 5, 6 ]
+    sliceUntil -3 (fromList (List.range 0 6))
+        == fromList [ 0, 1, 2, 3 ]
 
 -}
 sliceUntil : Int -> Array a -> Array a
-sliceUntil n a =
+sliceUntil n array =
     if n >= 0 then
-        slice 0 n a
+        slice 0 n array
 
     else
-        slice 0 (length a + n) a
+        slice 0 (length array + n) array
 
 
 {-| Remove the last element from an array.
 
     pop (fromList [ 1, 2, 3 ]) == fromList [ 1, 2 ]
 
+    pop empty == empty
+
 -}
 pop : Array a -> Array a
-pop arr =
-    slice 0 -1 arr
+pop array =
+    slice 0 -1 array
 
 
 {-| Apply a function that may succeed to all values in the array, but only keep the successes.
@@ -100,18 +113,18 @@ pop arr =
 
 -}
 filterMap : (a -> Maybe b) -> Array a -> Array b
-filterMap f xs =
-    xs
+filterMap tryMap array =
+    array
         |> Array.toList
-        |> List.filterMap f
+        |> List.filterMap tryMap
         |> Array.fromList
 
 
 {-| Apply an array of functions to an array of values.
 -}
 apply : Array (a -> b) -> Array a -> Array b
-apply fs xs =
-    map2 (\f b -> f b) fs xs
+apply maps array =
+    map2 (\f b -> f b) maps array
 
 
 {-| Apply a function to the array, collecting the result in a List.
@@ -122,8 +135,8 @@ This is useful for building HTML out of an array:
 
 -}
 mapToList : (a -> b) -> Array a -> List b
-mapToList f =
-    Array.foldr (f >> (::)) []
+mapToList alter =
+    Array.foldr (alter >> (::)) []
 
 
 {-| Apply a function to the array with the index as the first argument,
@@ -149,11 +162,13 @@ collecting the results in a List.
 
 -}
 indexedMapToList : (Int -> a -> b) -> Array a -> List b
-indexedMapToList f xs =
+indexedMapToList mapIndexAndValue array =
     Array.foldr
-        (\x ( i, ys ) -> ( i - 1, f i x :: ys ))
-        ( Array.length xs - 1, [] )
-        xs
+        (\x ( i, ys ) ->
+            ( i - 1, mapIndexAndValue i x :: ys )
+        )
+        ( Array.length array - 1, [] )
+        array
         |> Tuple.second
 
 
@@ -161,7 +176,8 @@ indexedMapToList f xs =
 If one array is longer, the extra elements are dropped.
 
     map2 (+) [ 1, 2, 3 ] [ 1, 2, 3, 4 ] == [ 2, 4, 6 ]
-    map2 Tuple.pair [ 1, 2, 3 ] [ 'a', 'b' ] == [ ( 1, 'a' ), ( 2, 'b' ) ]
+    map2 Tuple.pair [ 1, 2, 3 ] [ 'a', 'b' ]
+        == [ ( 1, 'a' ), ( 2, 'b' ) ]
 
     pairs : Array a -> Array b -> Array ( a, b )
     pairs lefts rights =
@@ -169,34 +185,54 @@ If one array is longer, the extra elements are dropped.
 
 -}
 map2 : (a -> b -> result) -> Array a -> Array b -> Array result
-map2 f ws =
-    Array.toList
-        >> List.map2 f (ws |> Array.toList)
-        >> Array.fromList
+map2 combineAb aArray bArray =
+    List.map2 combineAb
+        (aArray |> Array.toList)
+        (bArray |> Array.toList)
+        |> Array.fromList
 
 
 {-| -}
-map3 : (a -> b -> c -> result) -> Array a -> Array b -> Array c -> Array result
-map3 f ws xs =
-    apply (map2 f ws xs)
+map3 :
+    (a -> b -> c -> result)
+    -> Array a
+    -> Array b
+    -> Array c
+    -> Array result
+map3 combineAbc aArray bArray cArray =
+    apply (map2 combineAbc aArray bArray) cArray
 
 
 {-| -}
-map4 : (a -> b -> c -> d -> result) -> Array a -> Array b -> Array c -> Array d -> Array result
-map4 f ws xs ys =
-    apply (map3 f ws xs ys)
+map4 :
+    (a -> b -> c -> d -> result)
+    -> Array a
+    -> Array b
+    -> Array c
+    -> Array d
+    -> Array result
+map4 combineAbcd aArray bArray cArray dArray =
+    apply (map3 combineAbcd aArray bArray cArray) dArray
 
 
 {-| -}
-map5 : (a -> b -> c -> d -> e -> result) -> Array a -> Array b -> Array c -> Array d -> Array e -> Array result
-map5 f ws xs ys zs =
-    apply (map4 f ws xs ys zs)
+map5 :
+    (a -> b -> c -> d -> e -> result)
+    -> Array a
+    -> Array b
+    -> Array c
+    -> Array d
+    -> Array e
+    -> Array result
+map5 combineAbcde aArray bArray cArray dArray eArray =
+    apply (map4 combineAbcde aArray bArray cArray dArray) eArray
 
 
 {-| Take a predicate and an array, return an array that contains elements which fails to satisfy the predicate.
 This is equivalent to `Array.filter (not << predicate) list`
 
-    removeWhen isEven (fromList [ 1, 2, 3, 4 ]) == fromList [ 1, 3 ]
+    removeWhen isEven (fromList [ 1, 2, 3, 4 ])
+        == fromList [ 1, 3 ]
 
 -}
 removeWhen : (a -> Bool) -> Array a -> Array a
@@ -204,14 +240,19 @@ removeWhen pred xs =
     Array.filter (not << pred) xs
 
 
-{-| Zip arrays into tuples.
+{-| Zip the elements of two arrays into tuples.
+
+    zip [ 1, 2, 3 ] [ 'a', 'b' ]
+        == [ ( 1, 'a' ), ( 2, 'b' ) ]
+
 -}
 zip : Array a -> Array b -> Array ( a, b )
 zip =
     map2 Tuple.pair
 
 
-{-| -}
+{-| Zip the elements of three arrays into 3-tuples.
+-}
 zip3 : Array a -> Array b -> Array c -> Array ( a, b, c )
 zip3 =
     map3 (\a b c -> ( a, b, c ))
@@ -226,9 +267,9 @@ zip3 =
 
 -}
 unzip : Array ( a, b ) -> ( Array a, Array b )
-unzip arrAB =
-    ( arrAB |> Array.map Tuple.first
-    , arrAB |> Array.map Tuple.second
+unzip tupleArray =
+    ( tupleArray |> Array.map Tuple.first
+    , tupleArray |> Array.map Tuple.second
     )
 
 
@@ -312,7 +353,8 @@ resizerIndexed n f xs =
 
 {-| Reverse an array.
 
-    reverse (fromList [ 1, 2, 3 ]) == fromList [ 3, 2, 1 ]
+    reverse (fromList [ 1, 2, 3, 4 ])
+        == fromList [ 4, 3, 2, 1 ]
 
 -}
 reverse : Array a -> Array a
@@ -371,10 +413,7 @@ splitAt index xs =
 removeAt : Int -> Array a -> Array a
 removeAt index xs =
     -- TODO: refactor (written this way to help avoid Array bugs)
-    if index < 0 then
-        xs
-
-    else
+    if index >= 0 then
         let
             ( beforeIndex, startingAtIndex ) =
                 splitAt index xs
@@ -389,16 +428,20 @@ removeAt index xs =
             append beforeIndex
                 (slice 1 lengthStartingAtIndex startingAtIndex)
 
+    else
+        xs
+
 
 {-| Insert an element at the given index.
 
-    insertAt 0 'b' (fromList [ 'a', 'c' ]) == fromList [ 'b', 'a', 'c' ]
+    insertAt 1 'b' (fromList [ 'a', 'c' ])
+        == fromList [ 'a', 'b', 'c' ]
 
-    insertAt 1 'b' (fromList [ 'a', 'c' ]) == fromList [ 'a', 'b', 'c' ]
+    insertAt -1 'b' (fromList [ 'a', 'c' ])
+        == fromList [ 'a', 'c' ]
 
-    insertAt -1 'b' (fromList [ 'a', 'c' ]) == fromList [ 'a', 'c' ]
-
-    insertAt 10 'b' (fromList [ 'a', 'c' ]) == fromList [ 'a', 'c' ]
+    insertAt 100 'b' (fromList [ 'a', 'c' ])
+        == fromList [ 'a', 'c' ]
 
 -}
 insertAt : Int -> a -> Array a -> Array a
