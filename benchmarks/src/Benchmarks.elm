3e898ab703b4353143be310d2f3cbaee552a75ce
passi <pascalfeyer@gmail.com>
2021-05-29 15:08:57 +0200

    add map2 & filterMap benchmarks

--- benchmarks/src/Benchmarks.elm
+++ benchmarks/src/Benchmarks.elm
@@ -1,6 +1,7 @@
 module Benchmarks exposing (main)
 
 import Array exposing (Array)
+import Array.Extra as Array
 import Benchmark exposing (Benchmark, benchmark, describe)
 import Benchmark.Runner exposing (BenchmarkProgram)
 import Candidates exposing (..)
@@ -14,70 +15,116 @@ main =
 suite : Benchmark
 suite =
     describe "array extra"
-        [ Benchmark.compare "from 0 to 99"
-            "initialize"
-            (\() ->
-                Array.initialize 100 (\i -> i)
+        [ compare "from 0 to 99"
+            (\f -> f ())
+            ( "initialize"
+            , \() -> Array.initialize 100 (\i -> i)
             )
-            "fromList (List.range 0 _)"
-            (\() ->
-                Array.fromList (List.range 0 99)
+            ( "fromList (List.range 0 _)"
+            , \() -> Array.fromList (List.range 0 99)
             )
-        , Benchmark.compare "mapToList"
-            "foldr"
-            (\() ->
-                mapToListWithFoldr (\v -> -v)
-                    num1To100
-            )
-            "mapToList with Array toIndexedList"
-            (\() ->
-                mapToListWithListMap (\v -> -v)
-                    num1To100
-            )
+        , compare "mapToList"
+            (\mapToList -> mapToList (\v -> -v) ints1To100)
+            ( "with foldr", mapToListWithFoldr )
+            ( "with Array.toIndexedList", mapToListWithListMap )
         , let
-            compareToWithFoldr description function =
-                Benchmark.compare (description ++ " vs with Array.foldr")
-                    "with Array.foldr"
-                    (\() ->
-                        indexedMapToListWithFoldr Tuple.pair
-                            num1To100
+            compareToWithFoldr ( description, function ) =
+                compare (description ++ " vs with Array.foldr")
+                    (\indexedMapToList ->
+                        indexedMapToList Tuple.pair
+                            ints1To100
                     )
-                    description
-                    (\() ->
-                        function Tuple.pair
-                            num1To100
+                    ( "with Array.foldr"
+                    , indexedMapToListWithFoldr
                     )
+                    ( description, function )
           in
           describe "indexedMapToList"
-            [ compareToWithFoldr "with toIndexedList"
-                indexedMapToListWithToIndexedList
-            , compareToWithFoldr "with Array.indexedMap"
-                indexedMapToListWithArrayIndexedMap
-            , compareToWithFoldr "with List.indexedMap"
-                indexedMapToListWithListIndexedMap
+            [ compareToWithFoldr
+                ( "with toIndexedList"
+                , indexedMapToListWithToIndexedList
+                )
+            , compareToWithFoldr
+                ( "with Array.indexedMap"
+                , indexedMapToListWithArrayIndexedMap
+                )
+            , compareToWithFoldr
+                ( "with List.indexedMap"
+                , indexedMapToListWithListIndexedMap
+                )
             ]
         , let
-            compareToFoldlToList description function =
-                Benchmark.compare
+            compareToFoldlToList ( description, function ) =
+                compare
                     (description ++ " vs with Array.foldl to list")
-                    "with Array.foldl to list"
-                    (\() ->
-                        reverseWithFoldlToList num1To100
-                    )
-                    description
-                    (\() -> function num1To100)
+                    (\reverse -> reverse ints1To100)
+                    ( "with Array.foldl to list", reverseWithFoldlToList )
+                    ( description, function )
           in
           describe "reverse"
             [ compareToFoldlToList
-                "with List.reverse"
-                reverseWithListReverse
+                ( "with List.reverse", reverseWithListReverse )
             , compareToFoldlToList
-                "with Array.foldr to array"
-                reverseWithFoldl
+                ( "with Array.foldr to array", reverseWithFoldl )
             ]
+        , let
+            zipped =
+                Array.zip
+                    ints1To100
+                    ints1To100
+
+            compareToWithArrayMaps ( description, function ) =
+                compare "vs with Array.maps"
+                    (\unzip -> unzip zipped)
+                    ( "with Array.maps", unzipWithMaps )
+                    ( description, function )
+          in
+          describe "unzip"
+            [ compareToWithArrayMaps
+                ( "with List.unzip", unzipWithListUnzip )
+            , compareToWithArrayMaps
+                ( "with foldl to Arrays", unzipWithFoldlToArrays )
+            ]
+        , compare "map2"
+            (\map2 ->
+                map2 Tuple.pair
+                    ints1To100
+                    ints1To100
+            )
+            ( "with List.map2", map2WithListMap2 )
+            ( "with List.indexedMap", map2WithListIndexedMap )
+        , let
+            maybeInts =
+                Array.initialize 100
+                    (\x ->
+                        if (x |> modBy 3) == 0 then
+                            Nothing
+
+                        else
+                            Just x
+                    )
+          in
+          compare "filterMap"
+            (\filterMap -> filterMap identity maybeInts)
+            ( "with List.filterMap", filterMapWithListFilterMap )
+            ( "with push", filterMapWithPush )
         ]
 
 
-num1To100 : Array Int
-num1To100 =
+compare :
+    String
+    -> (a -> b_)
+    -> ( String, a )
+    -> ( String, a )
+    -> Benchmark
+compare name applyArguments ( aDescription, aFunction ) ( bDescription, bFunction ) =
+    Benchmark.compare name
+        aDescription
+        (\() -> applyArguments aFunction)
+        bDescription
+        (\() -> applyArguments bFunction)
+
+
+ints1To100 : Array Int
+ints1To100 =
     Array.fromList (List.range 1 100)
--- benchmarks/src/Candidates.elm
+++ benchmarks/src/Candidates.elm
@@ -53,3 +53,77 @@ mapToListWithFoldr alter =
 mapToListWithListMap : (a -> b) -> Array a -> List b
 mapToListWithListMap alter =
     Array.toList >> List.map alter
+
+
+unzipWithMaps : Array ( a, b ) -> ( Array a, Array b )
+unzipWithMaps tupleArray =
+    ( tupleArray |> Array.map Tuple.first
+    , tupleArray |> Array.map Tuple.second
+    )
+
+
+unzipWithListUnzip : Array ( a, b ) -> ( Array a, Array b )
+unzipWithListUnzip tupleArray =
+    tupleArray
+        |> Array.toList
+        |> List.unzip
+        |> Tuple.mapBoth Array.fromList Array.fromList
+
+
+unzipWithFoldlToArrays : Array ( a, b ) -> ( Array a, Array b )
+unzipWithFoldlToArrays arrAB =
+    Array.foldl
+        (\( a, b ) ( arrA, arrB ) ->
+            ( Array.push a arrA, Array.push b arrB )
+        )
+        ( Array.empty, Array.empty )
+        arrAB
+
+
+map2WithListMap2 : (a -> b -> c) -> Array a -> Array b -> Array c
+map2WithListMap2 combine aArray bArray =
+    List.map2 combine
+        (Array.toList aArray)
+        (Array.toList bArray)
+        |> Array.fromList
+
+
+map2WithListIndexedMap : (a -> b -> c) -> Array a -> Array b -> Array c
+map2WithListIndexedMap combine aArray bArray =
+    let
+        length =
+            min (Array.length aArray) (Array.length bArray)
+
+        aList =
+            Array.slice 0 length aArray
+                |> Array.toList
+    in
+    aList
+        |> List.indexedMap
+            (\i a ->
+                Maybe.map (combine a) (Array.get i bArray)
+            )
+        |> List.filterMap identity
+        |> Array.fromList
+
+
+filterMapWithPush : (a -> Maybe b) -> Array a -> Array b
+filterMapWithPush f xs =
+    let
+        maybePush : (a -> Maybe b) -> a -> Array b -> Array b
+        maybePush f_ mx xs_ =
+            case f_ mx of
+                Just x ->
+                    Array.push x xs_
+
+                Nothing ->
+                    xs_
+    in
+    Array.foldl (maybePush f) Array.empty xs
+
+
+filterMapWithListFilterMap : (a -> Maybe mapped) -> Array a -> Array mapped
+filterMapWithListFilterMap tryMap =
+    Array.toList
+        >> List.filterMap tryMap
+        >> Array.fromList
